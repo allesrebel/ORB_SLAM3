aaabdb465316fce219b1ef8dcab1ccafd99e49bd
rebel <allesrebel@yahoo.com>
2024-11-19 00:20:43 -0800

    Updated scripts

--- build.sh
+++ build.sh
@@ -38,5 +38,5 @@ echo "Configuring and building ORB_SLAM3 ..."
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-#cmake .. -DCMAKE_BUILD_TYPE=Debug
+#cmake .. -DCMAKE_BUILD_TYPE=Debug # -DNDEBUG # if you don't want asserts
 make -j
