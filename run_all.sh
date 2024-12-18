c128c8a08f03bfefd17607f60849e5fb67622717
rebel <allesrebel@yahoo.com>
2024-12-17 22:18:32 -0800

    updated the run_all

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# ORBSLAM3 Benchmarking Script
-# This script performs benchmarking on the MH01 - MH05 video datasets while collecting system-level and internal metrics of ORBSLAM3.
-
 # Get the current date for creating unique result directories and log files
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
@@ -12,23 +9,25 @@ run_orbslam() {
   local result_folder_prefix=$2
   local dataset=$3
   local run_number=$4
-  local log_file="cout_${result_folder_prefix}_${dataset}_${run_number}_${DATE}.log"
+  local mask_size=$5
+  local log_file="cout_${result_folder_prefix}_${dataset}_${mask_size}_${run_number}_${DATE}.log"
 
-  echo "Starting ORBSLAM3 run #${run_number} on dataset ${dataset} with configuration: $config_file"
   local dataset_with_underscore=$(echo $dataset | sed 's/\([A-Z]*\)\([0-9]*\)/\1_\2/')
   local command="./Examples/Stereo-Inertial/stereo_inertial_euroc ./Vocabulary/ORBvoc.txt $config_file ./Datasets/EuRoc/${dataset_with_underscore}* ./Examples/Stereo-Inertial/EuRoC_TimeStamps/${dataset}.txt dataset-${dataset}_stereo_imu"
   echo "Running command: $command"
-  $command 2>&1 | tee $log_file
+  $command > $log_file
 
   echo "Saving results..."
-  local result_folder="${result_folder_prefix}_${dataset}_${DATE}_run_${run_number}"
+  local result_folder="${DATE}_${result_folder_prefix}_${dataset}_${mask_size}_run_${run_number}"
   mkdir -p $result_folder
-  mv LocalMapTimeStats.txt ExecMean.txt f_dataset-${dataset}_stereo_imu.txt SessionInfo.txt kf_dataset-${dataset}_stereo_imu.txt LBA_Stats.txt TrackingTimeStats.txt $log_file $result_folder
+  mv LocalMapTimeStats.txt ExecMean.txt LBA_Stats.txt TrackingTimeStats.txt SessionInfo.txt $log_file $result_folder
 
-  # Move map_points.csv if it exists
-  if [ -f map_points.csv ]; then
-    mv map_points.csv $result_folder
-  fi
+  # Move additional output files if they exist
+  for file in map_points.csv "f_dataset-${dataset}_stereo_imu.txt" "kf_dataset-${dataset}_stereo_imu.txt"; do
+    if [ -f "$file" ]; then
+      mv "$file" "$result_folder"
+    fi
+  done
 
   echo "Results saved in $result_folder"
 }
@@ -47,13 +46,44 @@ CONFIGURATIONS=(
   "./Examples/Stereo-Inertial/EuRoC.yaml result_stereo_inertial_normal"
 )
 
-# Randomize dataset and configuration selection
-for ((i=1; i<=NUM_RUNS; i++)); do
-  for dataset in "${DATASETS[@]}"; do
-    for config in "${CONFIGURATIONS[@]}"; do
-      config_file=$(echo $config | awk '{print $1}')
-      result_folder_prefix=$(echo $config | awk '{print $2}')
-      run_orbslam $config_file $result_folder_prefix $dataset $i
-    done | shuf
-  done | shuf
+# Loop through configurations
+for config_pair in "${CONFIGURATIONS[@]}"; do
+  IFS=' ' read -r BASE_CONFIG RESULT_FOLDER_PREFIX <<< "$config_pair"
+
+  # Check if config name contains "fov" or "FOV"
+  if [[ $BASE_CONFIG == *"fov"* ]]; then
+    # Extract the base name of the config file without extension
+    FILENAME=$(basename "$BASE_CONFIG" .yaml)
+
+    # Loop through different mask sizes and create corresponding configurations
+    for ((mask_size=2; mask_size<=12; mask_size++)); do
+      CONFIG_FILE="./Examples/Stereo-Inertial/${FILENAME}_mask_${mask_size}x${mask_size}.yaml"
+      cp $BASE_CONFIG $CONFIG_FILE
+
+      # Update the maskHeight and maskWidth values in the copied YAML file
+      sed -i "s/^System.maskHeight: [0-9]*/System.maskHeight: ${mask_size}/" $CONFIG_FILE
+      sed -i "s/^System.maskWidth: [0-9]*/System.maskWidth: ${mask_size}/" $CONFIG_FILE
+
+      # Randomize dataset and configuration selection
+      for ((i=1; i<=NUM_RUNS; i++)); do
+        for dataset in "${DATASETS[@]}"; do
+          run_orbslam $CONFIG_FILE $RESULT_FOLDER_PREFIX $dataset $i $mask_size
+        done | shuf
+      done | shuf
+
+      # Remove the temporary configuration file after use
+      rm $CONFIG_FILE
+    done
+  else
+    # If not FOV-related, just run the base configuration as is
+    # with mask_size set to something like 0 to indicate no mask
+    mask_size=0
+    for ((i=1; i<=NUM_RUNS; i++)); do
+      for dataset in "${DATASETS[@]}"; do
+        run_orbslam $BASE_CONFIG $RESULT_FOLDER_PREFIX $dataset $i $mask_size
+      done
+    done
+  fi
+
 done
+
