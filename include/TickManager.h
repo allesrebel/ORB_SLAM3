98290d1d85495ba7f150ab752e9ef952980fa298
Rebel <allesrebel@yahoo.com>
2024-12-25 20:13:58 -0800

    src/TickManager.cc 	Fixed printing the stats all the time, no need 	frameBudget looks solid now
    include/TickManager.h
    	Updated to use a vector to contain information as it's collected
    		Information changed to per pyramid level:
    		row, columns, cell height and width
    
    	Updated some function defs to capture frame information for debugging
    		(actual frame number SLAM sees)
    
    src/Frame.cc
    	Updated all references to provide the frame information SLAM sees
    
    src/ORBextractor.cc
    	Updated to pass the per level info

--- include/TickManager.h
+++ include/TickManager.h
@@ -9,16 +9,32 @@ namespace ORB_SLAM3
 
 struct feature_extraction_settings_t
 {
+
+    struct pyramid_level_t
+    {
+        int nRows;    // number of rows in the grid at this level
+        int nCols;    // number of columns in the grid at this level
+        int cellWidth;  // width of each cell in the grid
+        int cellHeight; // height of each cell in the grid
+    };
+
     // These are the variables we need in order to figure out
     // how much time we have to provision to featurizing the frame
-    int nlevels;  // pyramid levels
-    int nCols;    // number of columns in the grid
-    int nRows;    // number of rows in the grid
+    std::vector<pyramid_level_t> pyramid_levels;
 
     // optional settings (TODO:)
     // bool enableFOV;
     // int maskHeight;
     // int maskWidth;
+};
+
+struct feature_extraction_info_t
+{
+    int level;
+    int nRows;
+    int nCols;
+    int cellWidth;
+    int cellHeight;
 };
 
 class TickManager 
@@ -37,16 +53,16 @@ public:
     static TickManager& getInstance();
 
     // Increment the frame's elapsed ticks, and extraction settings
-    void incrementTicks(feature_extraction_settings_t&);
+    void incrementTicks(const feature_extraction_info_t&&);
 
     // Signal the end of a frame and reset elapsed ticks, and actual time to do frame
-    void endFrame(double&);
+    void endFrame(long unsigned int&, double&);
 
     // Calculate average ticks per frame
     double getAverageTicksPerFrame() const;
 
     // Debug print
-    void printStats(double&) const;
+    void printStats(long unsigned int&, double&) const;
 
     // Delete copy constructor and assignment operator to enforce singleton pattern
     TickManager(const TickManager&) = delete;
